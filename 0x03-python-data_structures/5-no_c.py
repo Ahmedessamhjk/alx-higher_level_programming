5a807985b68b983ecfce56e3bda80b9215e06ef2
Ahmed <ahmedessam121026@icloud.com>
2023-09-11 08:39:48 +0300

    afgd

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    no_c = ""
+    no_C = ""
     for a in range(len(my_string)):
-        if (my_string[a] != 'c' and my_string != 'C'):
-            no_c += my_string[a]
-    return (no_c)
+        if (my_string[a] != 'c' and my_string[a] != 'C'):
+            no_C
+    return (no_C)
