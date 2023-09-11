1e7feeaeb6e319e3a8b5c6afc09aa7f37ccf94ec
Ahmed <ahmedessam121026@icloud.com>
2023-09-11 08:55:38 +0300

    sfdb

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,4 +2,4 @@
 def no_c(my_string):
     for a in range(len(my_string)):
         if (my_string[a] != 'c' and my_string[a] != 'C'):
-            return (mys_tring[a])
+            return (my_tring[a])
