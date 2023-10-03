16c8ebe661f3fa75f1feace8d5e13831cfbffeb4
Ahmed <ahmedessam121026@icloud.com>
2023-10-02 18:14:01 -0700

    asd

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''
-defining an empty class Rectangle
-'''
+"""
+Defining an empty class Rectangle
+"""
+
 
 class Rectangle:
-    ''' Empty representation of a Rectangle '''
+    """Empty representation of a Rectangle"""
     pass
